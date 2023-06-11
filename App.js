b332716e5ee4fed745b42cdb1a828f8b08ecf5d0
sahilm-10 <malgundkarsahil10@gmail.com>
2023-06-11 18:36:52 +0530

    Added Header Layout

--- App.js
+++ App.js
@@ -1,5 +1,23 @@
 import React from "react";
 import ReactDOM  from "react-dom/client";
+/*
+        Planning for UI layout
+        Header
+            -Logo
+            - Nav Items (Right side)
+            - Cart
+        Body
+         - Search Bar
+          -Restro list
+            - Restro card
+                - Image
+                - Name
+                -Rating
+                -Cusines
+        Footer
+            - links
+            - Copyright
+        */
 const heading = React.createElement(
     "h1",
     {
@@ -30,19 +48,39 @@ const heading2 = <h1 id="heading2" key="h2">Heading 2</h1>
 // Functional Component.
 
 const Title = () =>{
-    return(
-        <div>
-            {heading2}
-            <h1>This is a h1</h1>
-        </div>
+    return(   
+        <a href="/">
+        <img className="logo" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSfLK3kEJcq5i7QL17xo98YIzvb7-4iD0Yaag&usqp=CAU" alt="FoodVilla"/>
+        </a>
     );
 };
 
-const Title2 =()=>(
-    <div>
-        <h2>This is a h2</h2>
-    </div>
-);
+
+const HeaderComponent = () =>{
+    return(
+        <div className="header">
+        <Title/>
+        <div className="nav-items">
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+                <li>Cart</li>
+            </ul>
+        </div>
+        </div>
+    )
+    
+}
+
+
+const AppLayout = () =>{
+    return(
+       {}
+    );
+}
+
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Title/>);
+root.render(<HeaderComponent/>);
