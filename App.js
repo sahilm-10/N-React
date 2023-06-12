356f3491f925642939dddeda8f2fd9fee4c82c29
sahilm-10 <malgundkarsahil10@gmail.com>
2023-06-12 19:04:56 +0530

    Added Dynamic data-swiggy

--- App.js
+++ App.js
@@ -72,26 +72,394 @@ const HeaderComponent = () =>{
     
 }
 
-const RestroCard = ()=>{
+// Config driven UI 
+const restrolist =[
+    {
+        "type": "restaurant",
+        "data": {
+          "type": "F",
+          "id": "30072",
+          "name": "Golden Bake & Cakes",
+          "uuid": "2d7685df-ea29-4beb-9d12-9640704ee6d8",
+          "city": "4",
+          "area": "Dwarka",
+          "totalRatingsString": "5000+ ratings",
+          "cloudinaryImageId": "l3yced9vodwmvf1h2kkl",
+          "cuisines": [
+            "Bakery",
+            "Desserts",
+            "Snacks"
+          ],
+          "tags": [
+            
+          ],
+          "costForTwo": 40000,
+          "costForTwoString": "₹400 FOR TWO",
+          "deliveryTime": 24,
+          "minDeliveryTime": 24,
+          "maxDeliveryTime": 24,
+          "slaString": "24 MINS",
+          "lastMileTravel": 1.2999999523162842,
+          "slugs": {
+            "restaurant": "golden-bake-cakes-uttam-nagar-uttam-nagar",
+            "city": "delhi"
+          },
+          "cityState": "4",
+          "address": "67, Sewak Park, Near Metro Pillar 778, Dwarka Mod Metro Station, Uttam Nagar, New Delhi",
+          "locality": "Sector 15",
+          "parentId": 16097,
+          "unserviceable": false,
+          "veg": false,
+          "select": false,
+          "favorite": false,
+          "tradeCampaignHeaders": [
+            
+          ],
+          "ribbon": [
+            {
+              "type": "PROMOTED"
+            }
+          ],
+          "chain": [
+            
+          ],
+          "feeDetails": {
+            "fees": [
+              {
+                "name": "distance",
+                "fee": 3100,
+                "message": ""
+              },
+              {
+                "name": "time",
+                "fee": 0,
+                "message": ""
+              },
+              {
+                "name": "special",
+                "fee": 0,
+                "message": ""
+              }
+            ],
+            "totalFees": 3100,
+            "message": "",
+            "title": "Delivery Charge",
+            "amount": "3100",
+            "icon": ""
+          },
+          "availability": {
+            "opened": true,
+            "nextOpenMessage": "",
+            "nextCloseMessage": ""
+          },
+          "longDistanceEnabled": 0,
+          "rainMode": "NONE",
+          "thirdPartyAddress": false,
+          "thirdPartyVendor": "",
+          "adTrackingID": "cid=6920261~p=1~eid=00000188-afae-9eac-15ee-f44f00a60128",
+          "badges": {
+            "imageBased": [
+              
+            ],
+            "textBased": [
+              
+            ],
+            "textExtendedBadges": [
+              
+            ]
+          },
+          "lastMileTravelString": "1.2 kms",
+          "hasSurge": false,
+          "aggregatedDiscountInfoV3": {
+            "header": "40% OFF",
+            "subHeader": "UPTO ₹80",
+            "discountTag": "",
+            "headerTypeV2": 0
+          },
+          "sla": {
+            "restaurantId": "30072",
+            "deliveryTime": 24,
+            "minDeliveryTime": 24,
+            "maxDeliveryTime": 24,
+            "lastMileTravel": 1.2999999523162842,
+            "lastMileDistance": 0,
+            "serviceability": "SERVICEABLE",
+            "rainMode": "NONE",
+            "longDistance": "NOT_LONG_DISTANCE",
+            "preferentialService": false,
+            "iconType": "EMPTY"
+          },
+          "promoted": true,
+          "avgRating": "3.8",
+          "totalRatings": 5000,
+          "new": false
+        },
+        "subtype": "basic"
+      },
+      {
+        "type": "restaurant",
+        "data": {
+          "type": "F",
+          "id": "28451",
+          "name": "Grover Sweets & Bakers",
+          "uuid": "f4787efb-89ca-4954-bf69-df8f83cec00b",
+          "city": "4",
+          "area": "Uttam Nagar",
+          "totalRatingsString": "10000+ ratings",
+          "cloudinaryImageId": "at6kqassx47vpnzcpvwm",
+          "cuisines": [
+            "North Indian",
+            "Sweets",
+            "Street Food",
+            "Snacks",
+            "Indian",
+            "Tandoor",
+            "Desserts"
+          ],
+          "tags": [
+            
+          ],
+          "costForTwo": 30000,
+          "costForTwoString": "₹300 FOR TWO",
+          "deliveryTime": 30,
+          "minDeliveryTime": 30,
+          "maxDeliveryTime": 30,
+          "slaString": "30 MINS",
+          "lastMileTravel": 1.2999999523162842,
+          "slugs": {
+            "restaurant": "grover-sweets-bakers-uttam-nagar-uttam-nagar",
+            "city": "delhi"
+          },
+          "cityState": "4",
+          "address": "Plot no -1, Diwan Estate, Bhagwati Garden, Near Dwarka Mod Metro Station, Uttam Nagar, New Delhi",
+          "locality": "Bhagwati Garden",
+          "parentId": 91011,
+          "unserviceable": false,
+          "veg": true,
+          "select": false,
+          "favorite": false,
+          "tradeCampaignHeaders": [
+            
+          ],
+          "chain": [
+            
+          ],
+          "feeDetails": {
+            "fees": [
+              {
+                "name": "distance",
+                "fee": 3100,
+                "message": ""
+              },
+              {
+                "name": "time",
+                "fee": 0,
+                "message": ""
+              },
+              {
+                "name": "special",
+                "fee": 0,
+                "message": ""
+              }
+            ],
+            "totalFees": 3100,
+            "message": "",
+            "title": "Delivery Charge",
+            "amount": "3100",
+            "icon": ""
+          },
+          "availability": {
+            "opened": true,
+            "nextOpenMessage": "",
+            "nextCloseMessage": ""
+          },
+          "longDistanceEnabled": 0,
+          "rainMode": "NONE",
+          "thirdPartyAddress": false,
+          "thirdPartyVendor": "",
+          "adTrackingID": "",
+          "badges": {
+            "imageBased": [
+              
+            ],
+            "textBased": [
+              
+            ],
+            "textExtendedBadges": [
+              
+            ]
+          },
+          "lastMileTravelString": "1.2 kms",
+          "hasSurge": false,
+          "aggregatedDiscountInfoV3": {
+            "header": "40% OFF",
+            "subHeader": "UPTO ₹80",
+            "discountTag": "",
+            "headerTypeV2": 0
+          },
+          "sla": {
+            "restaurantId": "28451",
+            "deliveryTime": 30,
+            "minDeliveryTime": 30,
+            "maxDeliveryTime": 30,
+            "lastMileTravel": 1.2999999523162842,
+            "lastMileDistance": 0,
+            "serviceability": "SERVICEABLE",
+            "rainMode": "NONE",
+            "longDistance": "NOT_LONG_DISTANCE",
+            "preferentialService": false,
+            "iconType": "EMPTY"
+          },
+          "promoted": false,
+          "avgRating": "4.2",
+          "totalRatings": 10000,
+          "new": false
+        },
+        "subtype": "basic"
+      },
+      {
+        "type": "restaurant",
+        "data": {
+          "type": "F",
+          "id": "36694",
+          "name": "Sargam Sweets & Restaurant",
+          "uuid": "17e6ee59-7045-47a1-b902-66c2b1f8546a",
+          "city": "4",
+          "area": "Uttam Nagar",
+          "totalRatingsString": "10000+ ratings",
+          "cloudinaryImageId": "qetsckyabfqyq3qz4opm",
+          "cuisines": [
+            "Sweets",
+            "Snacks"
+          ],
+          "tags": [
+            
+          ],
+          "costForTwo": 30000,
+          "costForTwoString": "₹300 FOR TWO",
+          "deliveryTime": 26,
+          "minDeliveryTime": 26,
+          "maxDeliveryTime": 26,
+          "slaString": "26 MINS",
+          "lastMileTravel": 1.7999999523162842,
+          "slugs": {
+            "restaurant": "sargam-sweets-uttam-nagar-uttam-nagar",
+            "city": "delhi"
+          },
+          "cityState": "4",
+          "address": "A 11, Mohan Garden, Near Metro Pillar 753, Peepal Wala Road, Uttam Nagar, New Delhi",
+          "locality": "Pipal Wala Road",
+          "parentId": 16038,
+          "unserviceable": false,
+          "veg": false,
+          "select": false,
+          "favorite": false,
+          "tradeCampaignHeaders": [
+            
+          ],
+          "chain": [
+            
+          ],
+          "feeDetails": {
+            "fees": [
+              {
+                "name": "distance",
+                "fee": 3100,
+                "message": ""
+              },
+              {
+                "name": "time",
+                "fee": 0,
+                "message": ""
+              },
+              {
+                "name": "special",
+                "fee": 0,
+                "message": ""
+              }
+            ],
+            "totalFees": 3100,
+            "message": "",
+            "title": "Delivery Charge",
+            "amount": "3100",
+            "icon": ""
+          },
+          "availability": {
+            "opened": true,
+            "nextOpenMessage": "",
+            "nextCloseMessage": ""
+          },
+          "longDistanceEnabled": 0,
+          "rainMode": "NONE",
+          "thirdPartyAddress": false,
+          "thirdPartyVendor": "",
+          "adTrackingID": "",
+          "badges": {
+            "imageBased": [
+              
+            ],
+            "textBased": [
+              
+            ],
+            "textExtendedBadges": [
+              
+            ]
+          },
+          "lastMileTravelString": "1.7 kms",
+          "hasSurge": false,
+          "aggregatedDiscountInfoV3": {
+            "header": "₹125 OFF",
+            "subHeader": "ABOVE ₹249",
+            "discountTag": "FLAT DEAL",
+            "headerTypeV2": 0
+          },
+          "sla": {
+            "restaurantId": "36694",
+            "deliveryTime": 26,
+            "minDeliveryTime": 26,
+            "maxDeliveryTime": 26,
+            "lastMileTravel": 1.7999999523162842,
+            "lastMileDistance": 0,
+            "serviceability": "SERVICEABLE",
+            "rainMode": "NONE",
+            "longDistance": "NOT_LONG_DISTANCE",
+            "preferentialService": false,
+            "iconType": "EMPTY"
+          },
+          "promoted": false,
+          "avgRating": "3.7",
+          "totalRatings": 10000,
+          "new": false
+        },
+        "subtype": "basic"
+      },
+];
+
+
+const RestroCard = ({restaurant})=>{
+    // console.log(props);
     return(
         <div className="card">
-            <img src="https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/e33e1d3ba7d6b2bb0d45e1001b731fcf" alt="BurgerKing"/>
-            <h2>Burger King</h2>
-            <h3>Burgers,American</h3>
-            <h4>4,2 stars</h4>
+            <img src={"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_366/"+ restaurant.data?.cloudinaryImageId} alt="BurgerKing"/>
+            <h2>{restaurant.data?.name}</h2>
+            <h3>{restaurant.data?.cuisines.join(", ")}</h3>
+            <h4>{restaurant.data.lastMileTravelString} minutes</h4>
         </div>
     )
 }
 
+// You pass in arguments and you receive parameters!!!!!!!!!
+// props => properties
+
 const Body =()=>{
     return(
         <div className="restro-list">
-            <RestroCard/>
-            <RestroCard/>
-            <RestroCard/>
-            <RestroCard/>
-            <RestroCard/>
-            <RestroCard/>
+            <RestroCard restaurant={restrolist[0]}/>
+            <RestroCard restaurant={restrolist[1]}/>
+            <RestroCard restaurant={restrolist[2]}/>
+            <RestroCard restaurant={restrolist[0]}/>
+            <RestroCard restaurant={restrolist[1]}/>
+            <RestroCard restaurant={restrolist[2]}/>
         </div>
     )
 }
